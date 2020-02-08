ca2f7e7ce0a652b2dfc459d2efb6c8243b5a08d0
unknown <german.ospina-q@labinfo.is.escuelaing.edu.co>
2020-02-08 09:09:51 -0500

    Primera

---
+++ src/main/java/hangman/model/HangmanException.java
@@ -0,0 +1,10 @@
+package hangman.model;
+
+public class HangmanException extends Exception{
+	
+	public static String NO_NEGATIVOS = "No se permiten numero negativos"; 
+	
+	public HangmanException(String msg) {
+        super(msg);
+	}
+}
